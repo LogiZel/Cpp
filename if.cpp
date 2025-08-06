6aeab9aaf9e013951a89394f06530526193339ce
LogiZel <jizelprince@gmail.com>
2025-08-06 21:33:27 +0530

    Initial commit

---
+++ if.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int curval=0,val=0;
+    if (cin>>curval)
+    {
+        int cnt=1;
+        while (cin>>val)
+        {
+            if (val==curval)
+            {
+                ++cnt;
+            }
+            else
+            {
+                cout<<curval<<" occurs "<<cnt<<" times"<<endl;
+                curval=val;
+                cnt=1;
+            }
+        }
+        cout<<curval<<" occurs "<<cnt<<" times"<<endl;
+    }
+    return 0;
+}
